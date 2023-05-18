30749435ef26b10173ccecce2323602d44715827
Hoyong Jeong <71969575+ghdyd586@users.noreply.github.com>
2023-05-18 16:47:01 +0900

    Update 1449.py

--- Week 4/1449.py
+++ Week 4/1449.py
@@ -1,3 +1,17 @@
 import sys
 
-N,L = map(sys.stdin.readline().split())
+N,L = map(int, sys.stdin.readline().split())
+need = 0
+tmp = L
+K = list(map(int,sys.stdin.readline().split()))
+
+while 1 :
+    if (L >= N) :
+        need += 1
+        break
+    else :
+        need += 1
+        L *= 2
+
+
+print(need)
